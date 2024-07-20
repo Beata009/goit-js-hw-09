9a956d3cfd60cfa25ad59acb6fa4882cf649c53b
Beata009 <bkolodziejszymanska@gmail.com>
2024-07-20 21:27:28 +0200

    js mechanic

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+function getRandomHexColor() {  
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+function changeBackgroundColor() {  
+    const color = getRandomHexColor();
+    console.log('zmieniam kolor na', color);
+
+    document.body.style.backgroundColor = color;  
+}
+
+let interval;
+
+document.querySelector('[data-start]').addEventListener('click', function () {
+    console.log('klikniety start')
+    this.disabled = true;
+    document.querySelector('[data-stop]').disabled = false;
+    changeBackgroundColor();
+    interval=setInterval(changeBackgroundColor, 1000);
+});
+
+document.querySelector('[data-stop]').addEventListener('click', function(){
+    this.disabled = true;
+    document.querySelector('[data-start]').disabled = false;
+    clearInterval(interval);
+});
