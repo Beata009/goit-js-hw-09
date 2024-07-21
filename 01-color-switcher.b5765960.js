874430968337604753db3b921d2e58634966fa85
Beata009 <146775696+Beata009@users.noreply.github.com>
2024-07-21 13:25:53 +0000

    Deploying to gh-pages from @ Beata009/goit-js-hw-09@b5070840d0bf1f6827676b0c17a1b8a6f4139f6b 🚀

---
+++ 01-color-switcher.b5765960.js
@@ -0,0 +1,2 @@
+function t(){const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;console.log("zmieniam kolor na",t),document.body.style.backgroundColor=t}let e;document.querySelector("[data-start]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-stop]").disabled=!1,t(),e=setInterval(t,1e3)})),document.querySelector("[data-stop]").addEventListener("click",(function(){this.disabled=!0,document.querySelector("[data-start]").disabled=!1,clearInterval(e)}));
+//# sourceMappingURL=01-color-switcher.b5765960.js.map
